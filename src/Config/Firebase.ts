d8f43da8adffb1aae26ba52d420265fa0124bc28
yair8520 <yair8520@gmail.com>
2023-06-12 12:58:05 +0300

    chcnge to .env file

--- src/Config/Firebase.ts
+++ src/Config/Firebase.ts
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 const firebaseConfig = {
-  apiKey: 'AIzaSyAf8kJbl-sU74haSt0ENQyEQ4FSnD1lG_M',
-  authDomain: 'dev-tools-dbbae.firebaseapp.com',
-  projectId: 'dev-tools-dbbae',
-  storageBucket: 'dev-tools-dbbae.appspot.com',
-  messagingSenderId: '507877801135',
-  appId: '1:507877801135:web:c04922ac3d4b45e843e85a',
-  measurementId: 'G-WTVR1E4N9F',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 const firebaseReference = firebase.initializeApp(firebaseConfig);
 const auth = firebaseReference.auth();
